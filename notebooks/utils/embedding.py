b2c7f7eee09a993a961eca944c97fdd444d7db82
Larraguibel <dlarraguibel@uc.cl>
2025-11-14 17:46:36 -0300

    Added functionalities

--- notebooks/utils/embedding.py
+++ notebooks/utils/embedding.py
@@ -4,23 +4,11 @@ import os
 from google import genai
 import numpy as np
 import torch
-from torch.utils.data import Dataset
 
 
 BATCH_SIZE = 100
 MODEL_NAME = "text-embedding-004"
 
-
-class EmbeddingDataset(Dataset):
-    def __init__(self, X, y):
-        self.X = torch.tensor(X, dtype=torch.float32)
-        self.y = torch.tensor(y, dtype=torch.float32)
-
-    def __len__(self):
-        return len(self.X)
-
-    def __getitem__(self, idx):
-        return self.X[idx], self.y[idx]
     
 
 def embed_texts_in_batches(
